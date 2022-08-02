9da1edcab73829088f21df0f2ba80e8a947f570d
Akeva Melchor <akeva.melchor@expel.io>
2022-08-02 14:53:32 -0700

    add routes

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       // log where we can go to test our GQL API
-      console.log(`Use GraphQL at http://127.0.0.1:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
 };
